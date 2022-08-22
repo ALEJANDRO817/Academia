5d4ce63819e544fa438511864ed147cfdaa76d45
ALEJANDRO817 <aramirez817@misena.edu.co>
2022-08-22 09:51:24 -0500

    En este espacio trabajado en equipo se integraron nuevas migraciones

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -102,7 +102,7 @@ class TeacherController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(storeTeacherRequest $request, $id)
     {
         $tutor = Teacher::find($id);
 
