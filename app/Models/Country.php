43df5eaddbec73a92baabcf8a35b4acc3674685f
ALEJANDRO817 <aramirez817@misena.edu.co>
2022-08-25 10:27:12 -0500

    Actualizacion

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -7,5 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Country extends Model
 {
+    protected $fillable = ['name'];
     use HasFactory;
 }
--- database/migrations/2022_08_22_085114_create_countries_table.php
+++ database/migrations/2022_08_22_085114_create_countries_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('countries', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('name');
         });
     }
 
