fe92c8118d1edfd8f94b61940f579b93edfe0e7d
ALEJANDRO817 <aramirez817@misena.edu.co>
2022-09-05 09:55:30 -0500

    Se agrego comentario

--- resources/views/students/add_student.blade.php
+++ resources/views/students/add_student.blade.php
@@ -7,5 +7,6 @@
     <div class="alert alert-success mt-5 text-center" role="alert">
         <p clas="text-center"><b>Se ha agregado Estudiante</b></p>
     </div>
+    {{-- cooomment --}}
 
 @endsection
